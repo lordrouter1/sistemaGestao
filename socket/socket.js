abe5f2e5068f93ac0ba293a701151204b047f387
sys7375 <juliobenin@yahoo.com.br>
2023-04-24 18:10:53 -0300

    configurado socket edit cliente

--- socket/socket.js
+++ socket/socket.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require("mongodb");
+
 function convertToObj(data){
     let ret = new Object();
     for(let i = 0; i < data.length;i++){
@@ -29,7 +31,28 @@ module.exports = (io,socket,con) => {
 
     socket.on('editUsr',(_data)=>{
         let data = convertToObj(JSON.parse(_data['form']));
-        console.log(data,_data['id']);
-        socket.emit('editUser-resp',{success:true});
+        let id = new ObjectId(_data['id']);
+
+        try{
+            con.collection('usuarios').findOne((data['inp_cnpj']==undefined)?{inp_cpf:data['inp_cpf'],_id:{$ne:id}}:{inp_cnpj:data['inp_cnpj'],_id:{$ne:id}}).then((r)=>{
+                if(r == null){
+                    con.collection('usuarios').updateOne({_id:id},{$set:data},(err,res)=>{
+                        console.log(err,res);
+                        socket.emit('editUser-resp',{success:true});
+                    });
+                }else{
+                    socket.emit('editUser-resp',{success:false,err:'CPF ou CNPJ já cadastrado!'});
+                }
+            });
+        }catch(e){
+            console.log(e);
+            socket.emit('editUser-resp',{success:false,err:'Erro interno ao salvar!'});
+        }
+    });
+
+    socket.on('delUsr',(_data)=>{
+        let data = _data;
+        console.log(data);
+        socket.emit('delUser-resp',{success:true});
     });
 }
