d27fda5ccf8c75fb899164c3064d4768e3f53a70
lordrouter1 <juliobenin@yahoo.com.br>
2023-05-18 09:40:17 -0300

    alterações dashboard

--- public/js/views/dashboard/functions.js
+++ public/js/views/dashboard/functions.js
@@ -1,7 +1,30 @@
+let chartOpt = {
+    plugins:{
+        legend:{
+            display: false,
+        },
+    },
+    scales: {
+        y: {
+            display: false
+        },
+        x: {
+            display: false
+        }
+    }
+}
+let doughnutOpt = {
+    plugins:{
+        legend:{
+            display: false,
+        }
+    }
+}
+
 var chart1 = new Chart($('#chart_1'),{
     type:'doughnut',
     data: {
-        labels: ['Pedidos', 'Orçamentos'],
+        labels: ['Abertos', 'Orçamentos'],
         datasets: [
             {
                 data: [34,7],
@@ -10,11 +33,68 @@ var chart1 = new Chart($('#chart_1'),{
             },
         ]
     },
-    options:{
-        plugins:{
-            legend:{
-                display: false,
-            }
-        }
-    }
+    options:doughnutOpt
+});
+
+var chart2 = new Chart($('#chart_2'),{
+    type:'line',
+    data: {
+        labels: ['Janeiro','Fevereiro','Março','Abril'],
+        datasets: [
+            {
+                data: [34,37,28,25],
+                backgroundColor: '#295872',
+                fill: false,
+                tension: 0.1
+            },
+        ]
+    },
+    options: chartOpt,
+});
+
+var chart3 = new Chart($('#chart_3'),{
+    type:'line',
+    data: {
+        labels: ['Janeiro','Fevereiro','Março','Abril'],
+        datasets: [
+            {
+                data: [14,32,40,56],
+                backgroundColor: '#295872',
+                fill: false,
+                tension: 0.1,
+            },
+        ]
+    },
+    options: chartOpt
+});
+
+var chart4 = new Chart($('#chart_4'),{
+    type:'doughnut',
+    data: {
+        labels: ['Abertas', 'Vencidas'],
+        datasets: [
+            {
+                data: [20,9],
+                borderWidth: 1,
+                backgroundColor: ['#295872','#b5d3e3']
+            },
+        ]
+    },
+    options:doughnutOpt
+});
+
+var chart5 = new Chart($('#chart_5'),{
+    type:'line',
+    data: {
+        labels: ['Janeiro','Fevereiro','Março','Abril'],
+        datasets: [
+            {
+                data: [150,204,365,382],
+                backgroundColor: '#295872',
+                fill: false,
+                tension: 0.1,
+            },
+        ]
+    },
+    options: chartOpt
 });
