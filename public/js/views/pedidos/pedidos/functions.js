2d5d7d4ebcdfc5b2b80c682aaa1278cd5594942f
lordrouter1 <juliobenin@yahoo.com.br>
2023-08-11 10:41:50 -0300

    editando pedidos

--- public/js/views/pedidos/pedidos/functions.js
+++ public/js/views/pedidos/pedidos/functions.js
@@ -7,5 +7,12 @@ $(document).ready(function(){
     $(document).on(`click`,`.delButton`,function(t){
         $(t.currentTarget).parent().parent().remove();
     });
+
+    $("#multi-state-toggle .btn").click(function () {
+        $("#multi-state-toggle .btn").addClass("btn-secondary");
+        $("#multi-state-toggle .btn").removeClass("btn-light");
+        $(this).addClass("btn-light");
+        $(this).removeClass("btn-secondary");
+    });
 });
     
