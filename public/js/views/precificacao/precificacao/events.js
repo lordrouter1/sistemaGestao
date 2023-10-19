a3aabec1ae723942034eb27c379fdef68bef975a
lordrouter1 <juliobenin@yahoo.com.br>
2023-10-19 16:00:55 -0300

    edição em precificação

--- public/js/views/precificacao/precificacao/events.js
+++ public/js/views/precificacao/precificacao/events.js
@@ -32,14 +32,13 @@ Swal.fire({
 $(document).ready(function(){
 
     $(document).on('change','.produto',function(self){
-        let root = $(this).parent().parent().parent()
+        let root = $(this).parent().parent().parent();
         $(root.find('.varList')).empty();
         $(root.find('.btnAdicionar')).show();
         $(root.find('.nome')).val(root.find('.produto option:selected').text());
         if($(this).val() != ''){
             fetch(`/precificacao/get/variacoes/${$(this).val()}/${$('[name="csrfToken"]').val()}`).then(r=>r.json()).then(r=>{
                 root.attr('cont',0);
-                console.log(r);
                 root.attr('data',JSON.stringify(r));
                 novaVar(root);
                 $('#btnAdicionar').show();
@@ -47,8 +46,18 @@ $(document).ready(function(){
         }
     });
 
+    $(document).on('click','.btnAdicionar',function(){
+        novaVar($(this).parent());
+    });
+
     $('#addProd').click(function(){
-        $('#prodModel .dProd').clone().appendTo('#prodList');
+        let cont = $('#prodList').attr('cont');
+        let novo = $('#prodModel .dProd').clone();
+        novo.find('[nome]').attr('name',`produto[${cont}][nome]`);
+        novo.find('[produto]').attr('name',`produto[${cont}][codigo]`);
+        novo.attr('codigo',cont);
+        novo.appendTo('#prodList');
+        $('#prodList').attr('cont',++cont);
     });
 
 });
--- public/js/views/precificacao/precificacao/functions.js
+++ public/js/views/precificacao/precificacao/functions.js
@@ -16,8 +16,8 @@ $(document).ready(function(){
     sortRows()
 });
 
-function sortRows(){
-    $("#variacoesHeader").sortable({
+function sortRows(root){
+    $(root.find('.varList')).sortable({
         items: ".stripedRow",
         tolerance: "pointer",
         cursor: "move",
@@ -36,19 +36,19 @@ function sortRows(){
 }
 
 function novaVar(root){
-    console.log(JSON.parse(root.attr('data')));
     let r = JSON.parse(root.attr('data'));
     let cont = root.attr('cont');
+    let produto = root.attr('codigo'); 
     let temp = $('<div class="row p-2 stripedRow"></div>');
     r.forEach(item => {
-        temp.append(`<div class="col"><select class="form-control" name="variacao[${cont}][variacoes][]" required><option selected disabled>${item['nome']}</option>${item['var'].map(v => `<option value="${item['_id']}:${v._id}">${v.nome}</option>`)}</select></div>`);
+        temp.append(`<div class="col"><select class="form-control" name="produto[${produto}][variacao][${cont}][variacoes][]" required><option selected disabled>${item['nome']}</option>${item['var'].map(v => `<option value="${item['_id']}:${v._id}">${v.nome}</option>`)}</select></div>`);
     });
-    temp.append(`<div class="col"><input type="text" name="variacao[${cont}][preco]" placeholder="Preço" class="form-control" required></div>`);
-    temp.append(`<div class="col"><input type="text" name="variacao[${cont}][minPreco]" placeholder="Preço mínimo" class="form-control"></div>`);
+    temp.append(`<div class="col"><input type="text" name="produto[${produto}][variacao][${cont}][preco]" placeholder="Preço" class="form-control" required></div>`);
+    temp.append(`<div class="col"><input type="text" name="produto[${produto}][variacao][${cont}][minPreco]" placeholder="Preço mínimo" class="form-control"></div>`);
     temp.append(`<div class="col-1 d-flex"><button type="button" class="btn btn-danger delButton"><i class="fa-solid fa-trash-can"></i></button><i class=" ml-auto mb-auto mt-auto text-primary fa-solid fa-sort"></i></div>`);
 
     $(root.find('.varList')).append(temp);
     root.attr('cont',++cont);
-    //sortRows();
+    sortRows(root);
 }
     
